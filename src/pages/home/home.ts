00dc1e616a91af5a0f6b417c8750be82a4e12706
Alessandro Loia <30357682+alexdloia@users.noreply.github.com>
2017-09-30 19:01:14 -0500

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,53 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  currentTime: string;
   constructor(public navCtrl: NavController) {
+    this.currentTime = this.calculateTime('-6');
+
+
+    // If it is Daylight Savings Time
+    if (this.dst(new Date())) {
+      this.currentTime = this.calculateTime('-5');
+    }
+  }
+  calculateTime(offset: any) {
+    // create Date object for current location
+    let d = new Date();
+
+    // create new Date object for different city
+    // using supplied offset
+    let nd = new Date(d.getTime() + (3600000 * offset));
+
+    return nd.toISOString();
+  }
+
+  stdTimezoneOffset(today: any) {
+    let jan = new Date(today.getFullYear(), 0, 1);
+    let jul = new Date(today.getFullYear(), 6, 1);
+    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+  }
+
+  dst(today: any) {
+    return today.getTimezoneOffset() < this.stdTimezoneOffset(today);
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+    this.currentTime = this.calculateTime('-6');
+
+
+    // If it is Daylight Savings Time
+    if (this.dst(new Date())) {
+      this.currentTime = this.calculateTime('-5');
+    }
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
 
   }
 
-}
+
+
