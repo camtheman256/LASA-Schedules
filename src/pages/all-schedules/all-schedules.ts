911d78e341eb9cd003916fe630dd68d98ca073dd
Cameron Kleiman <camtheman256@gmail.com>
2017-10-03 14:11:08 -0500

    Added a schedule and tabular display

--- src/pages/all-schedules/all-schedules.ts
+++ src/pages/all-schedules/all-schedules.ts
@@ -14,7 +14,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'all-schedules.html',
 })
 export class AllSchedulesPage {
-
+  public schedules: Array<Object> = [
+    {
+      name: "Standard",
+      schedule: [{
+        name: "1/5",
+        startTime: "8:30",
+        endTime: "10:00"
+      },
+      {
+        name: "2/6",
+        startTime: "10:05",
+        endTime: "11:40"
+      },
+      {
+        name: "Lunch",
+        startTime: "11:40",
+        endTime: "12:35"
+      },
+      {
+        name: "3/7",
+        startTime: "12:40",
+        endTime: "14:10",
+      },
+      {
+        name: "4/8",
+        startTime: "14:15",
+        endTime: "15:45"
+      }]
+    }
+  ];
+  schedPicker: string = this.schedules[0]["name"];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
