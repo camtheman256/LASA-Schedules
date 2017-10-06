ef51b6a20aad56ea4cf30bdb421aacb11874292c
alexdloia <30357682+alexdloia@users.noreply.github.com>
2017-10-06 11:33:39 -0500

    Added weekend recognition and fixed grammar.

--- src/pages/current-schedule/current-schedule.ts
+++ src/pages/current-schedule/current-schedule.ts
@@ -45,7 +45,7 @@ export class CurrentSchedulePage {
       this.now = new Date();
       this.currentTime = this.now.toLocaleTimeString([],{hour12: this.twentyfour != null ? !this.twentyfour : false});
       out = this.periodCheck(this.now.toLocaleTimeString([],{hour12: false}), this.currentSchedule);
-      this.currentPeriod = out ? out : "No school. " + this.timeToNextSchoolDay(this.now.toLocaleTimeString([], {hour12: false}), this.currentSchedule) + " left before schools begins.";
+      this.currentPeriod = out ? out : "No school. " + this.timeToNextSchoolDay(this.now.toLocaleTimeString([], {hour12: false}), this.currentSchedule) + " left before school begins.";
     }, 500);
 
   }
@@ -79,6 +79,10 @@ export class CurrentSchedulePage {
       }
     });
     this.timeRemaining = timeLeft;
+    if(this.now.getDay() === 0 || this.now.getDay() === 6) {
+      itOut = "It's the weekend! Enjoy!";
+      this.timeRemaining = null;
+    }
     return itOut;
   }
   timeToNextSchoolDay(now:string, schedIndex:number):string {
